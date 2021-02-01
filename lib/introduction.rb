0257e8d9f61d13ae5fade2d64c28ac7a5dd1dafa
Mathew E Newton <elijah2895@hotmail.com>
2021-02-01 14:49:50 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -16,7 +16,7 @@ end
 introduction_with_language("Josh", "React")
 
 def introduction_with_language_optional(name, language = "Ruby")
-  puts "Hi, my name is #{name} and I am leaning to program in #{language}."
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
 introduction_with_language_optional("Edwin", "Python")
